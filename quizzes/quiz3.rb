df5973be83e566262d370909c8ed7a65de04a5b9
cxm6467@gmail.com <cxm6467@gmail.com>
2017-08-30 20:07:36 +0000

    Done

---
+++ quizzes/quiz3.rb
@@ -0,0 +1,16 @@
+class Car 
+  attr_accessor :tires, :doors
+
+  def initialize(tires, doors)
+    self.tires = tires
+    self.doors = doors
+  end
+
+  def output_car
+    puts "This car has #{self.tires} tires and #{self.doors} doors."
+  end
+end
+
+
+car = Car.new(4,4)
+car.output_car
