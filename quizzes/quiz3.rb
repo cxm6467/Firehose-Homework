46d28f4fcef74d60383d1a5605bffe375329df91
cxm6467@gmail.com <cxm6467@gmail.com>
2017-09-02 18:56:31 +0000

    Initial commit

--- quizzes/quiz3.rb
+++ quizzes/quiz3.rb
@@ -1,6 +1,5 @@
 class Car 
   attr_accessor :tires, :doors
-
   def initialize(tires, doors)
     self.tires = tires
     self.doors = doors
@@ -10,7 +9,6 @@ class Car
     puts "This car has #{self.tires} tires and #{self.doors} doors."
   end
 end
-
 
 car = Car.new(4,4)
 car.output_car
