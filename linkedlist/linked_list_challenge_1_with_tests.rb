d6f99407520d2208db5181d977559baaa8d9f8bd
cxm6467@gmail.com <cxm6467@gmail.com>
2017-09-02 18:57:44 +0000

    Tests

--- linkedlist/linked_list_challenge_1_with_tests.rb
+++ linkedlist/linked_list_challenge_1_with_tests.rb
@@ -6,3 +6,15 @@ class LinkedListNode
     @next_noed = next_node
   end
 end
+
+
+### Test ###
+RSpec.describe Stack do
+  describe '#pop' do
+    it 'will return nil when empty'
+    it 'will return the last item pushed onto the stack'
+    it 'will return nil if I pushed once then pop twice'
+  describe '#push' do
+    it 'will add to the top of the stack'
+  end
+end
