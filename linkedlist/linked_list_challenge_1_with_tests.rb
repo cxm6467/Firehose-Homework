91519843876f5a4d5f9b0435750867fe8bcadc18
cxm6467@gmail.com <cxm6467@gmail.com>
2017-09-02 19:10:47 +0000

    Base code

--- linkedlist/linked_list_challenge_1_with_tests.rb
+++ linkedlist/linked_list_challenge_1_with_tests.rb
@@ -1,3 +1,4 @@
+## Linked listnode creates nodes
 class LinkedListNode
   attr_accessor :value, :next_node
 
@@ -7,6 +8,51 @@ class LinkedListNode
   end
 end
 
+## print values in a list
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+## Stack to help reverse a list
+class Stack
+    attr_reader :data
+
+    def initialize
+        @data = nil
+    end
+
+    # Push a value onto the stack
+    def push(value)
+        # IMPLEMENT ME!
+    end
+
+    # Pop an item off the stack.
+    # Remove the last item that was pushed onto the
+    # stack and return the value to the user
+    def pop
+        # I RETURN A VALUE
+    end
+
+end
+
+## Reverse a given list
+def reverse_list(list)
+    # ADD CODE HERE
+
+    while list
+        # ADD CODE HERE
+        list = list.next_node
+    end
+
+    # ADD CODE HERE
+end
+
 
 ### Test ###
 RSpec.describe Stack do
@@ -14,7 +60,8 @@ RSpec.describe Stack do
     it 'will return nil when empty'
     it 'will return the last item pushed onto the stack'
     it 'will return nil if I pushed once then pop twice'
-  describe '#push' do
-    it 'will add to the top of the stack'
+    describe '#push' do
+      it 'will add to the top of the stack'
+    end
   end
 end
