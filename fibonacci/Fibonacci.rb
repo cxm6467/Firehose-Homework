5025a663a7ece453ef96671c417452f430ed743c
cxm6467@gmail.com <cxm6467@gmail.com>
2017-09-25 20:45:16 +0000

    I need to look into this further but it works.

--- fibonacci/Fibonacci.rb
+++ fibonacci/Fibonacci.rb
@@ -1,16 +1,37 @@
+
+
+def recursive_fib(n)
+   if n == 1 || n == 2 
+    return 1
+  else
+    recursive_fib(n-1)+recursive_fib(n-2)
+  end
+end
+
+def iterative_fib(n)
+  if n == 1 || n == 2 
+    return 1
+  else
+    previous_value = 0
+    current_value = 1
+
+    n.times do
+      interim = previous_value
+      previous_value = current_value
+      current_value = interim + current_value
+    end
+    return previous_value
+  end
+end
+
+#  iterative_fib(10)
+# puts "Iterative:  #{iterative_fib(10)}"
+# puts "Recursive:  #{recursive_fib(10)}"
+
 require 'benchmark'
 
-def recursive_fib
-end
-
-def iterative_fib
-end
-
-
-
-
 ## Benchmark Tests
-num = 35
+num = 40
 Benchmark.bm do |x|
   x.report("recursive_fib") { recursive_fib(num) }
   x.report("iterative_fib")  { iterative_fib(num)  }
