5b615f33dc8a46c8f5cecd9c8c451ae8e465f3ee
cxm6467@gmail.com <cxm6467@gmail.com>
2017-10-15 14:30:39 +0000

    Recursive

--- collatz/collatz.rb
+++ collatz/collatz.rb
@@ -1,51 +1,46 @@
-@cache = {}
-## If I've enocuntered this number get it from the cache { name, value }
-## If key/value exists, grab it from cache, if not add it
-
+@cache = {1 => 1}
 
 def collatz(n,length=0)
-  return length + 1 if n == 1 
+  return @cache[n] + length if @cache.key?(n)
+  # return length + 1 if n == 1
   next_num = 0
+  length_of_n = 0
 
-  # while n != 1
     if n.even?
-      # puts "n \"#{n}\" is even, computing with n/2."
       next_num = n/2
-      # puts "Next Number is:  #{next_num}"
-      # puts "Continuing to next in sequence."
-      collatz(next_num,length + 1)
+      length_of_n = collatz(next_num,length + 1)
     else
-      # puts "n \"#{n}\" is odd, computing with 3n + 1."
       next_num = (3 * n ) + 1
-      # puts "Next Number is:  #{next_num}"
-      # puts "Incrementing count to #{length} ."
-      collatz(next_num, length + 1)
+      length_of_n = collatz(next_num, length + 1)
     end
-  # end
+
+    @cache[next_num] = length_of_n - 1
+    return length_of_n
 end
 
 largest_collatz = 0
 number_with_longest_collatz = 0
 
-(1..1_000_000).each do |i|
+(1..3).each do |i|
   current_collatz = collatz(i)
 
   if current_collatz > largest_collatz
-    largest_collatz = current_collatz 
-    number_with_longest_collatz = i 
+    largest_collatz = current_collatz
+    number_with_longest_collatz = i
   end
 
 end
 
 puts number_with_longest_collatz
-
+puts @cache.inspect
 # puts collatz(1)
 # puts collatz(2)
-# puts collatz(3)
+puts collatz(3)
 # puts collatz(4)
 # puts collatz(5)
 # puts collatz(6)
 # puts collatz(7)
 # puts collatz(1_000_000)
 
-## Cache/Benchmark of cache no cache
+
+@cache[n] ||= @cache[next_num] + 1
