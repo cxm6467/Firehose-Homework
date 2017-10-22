2beb9dda780bf88dc0d1db888ca126e6b75ba116
cxm6467@gmail.com <cxm6467@gmail.com>
2017-10-22 14:46:27 +0000

    Breadth

--- tree_traversal_breadth.rb
+++ tree_traversal_breadth.rb
@@ -12,19 +12,49 @@ class MyQueue
   end
 end
 
-queue = MyQueue.new
+class Tree
+  attr_accessor :payload, :children
 
-queue.enqueue(1)
-queue.enqueue(2)
-puts queue.dequeue
-queue.enqueue(3)
-queue.enqueue(4)
-queue.enqueue(5)
-puts queue.dequeue
-queue.enqueue(6)
-puts queue.dequeue
-puts queue.dequeue
-puts queue.dequeue
-puts queue.dequeue
+  def initialize(payload, children)
+    @payload = payload
+    @children = children
+  end
+end
 
-# Outputs: 1, 2, 3, 4, 5, 6
+
+# The "Leafs" of a tree, elements that have no children
+deep_fifth_node = Tree.new(5, [])
+eleventh_node = Tree.new(11, [])
+fourth_node   = Tree.new(4, [])
+
+# The "Branches" of the tree
+ninth_node = Tree.new(9, [fourth_node])
+sixth_node = Tree.new(6, [deep_fifth_node, eleventh_node])
+seventh_node = Tree.new(7, [sixth_node])
+shallow_fifth_node = Tree.new(5, [ninth_node])
+
+# The "Trunk" of the tree
+trunk = Tree.new(2, [seventh_node, shallow_fifth_node])
+
+def traverse(node, payload_to_find)
+  # return node if node.payload == payload_to_find
+  # node.children.each do |child|
+  #   is_found = traverse(child,payload_to_find)
+  #   return is_found unless is_found.nil?
+  # end
+  # return nil
+  queue = MyQueue.new
+  queue.enqueue(node)
+  while current_node = queue.dequeue
+    return current_node if current_node.payload == payload_to_find
+    current_node.children.each do |child|
+      queue.enqueue(child)
+    end
+  end
+  return nil
+end
+
+puts traverse(trunk, 2) == trunk
+puts traverse(trunk, 7) == seventh_node
+puts traverse(trunk, 5) == shallow_fifth_node
+puts traverse(trunk, 5) != deep_fifth_node
